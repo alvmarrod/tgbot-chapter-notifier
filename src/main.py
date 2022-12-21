67f0345db826b8ea0ddeb9e0bb3734c242ff0a6e
Alvaro MR <btc@hotmail.es>
2022-12-21 13:54:32 +0900

    Rename token placeholder

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ import app.log
 import app.bot as bot
 
 if __name__ == '__main__':
-    bot_token: str = 'placeholder'
+    bot_token: str = 'BotFather_provided_token'
     app = bot.build_basic_bot(bot_token)
     bot.add_actions_v1(app)
     bot.run_bot(app)
